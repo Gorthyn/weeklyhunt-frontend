cd203d59e353d081685959d245f6585168f6dc7b
Dylan Kline <dylanekline@gmail.com>
2024-08-23 15:59:05 -0500

    Setup React Router and basic routes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css'; './styles/App.css';
+import './App.css';
+import './styles/App.css';
 import HelloWorld from './components/HelloWorld';
 import Header from './components/Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
 
-      {/* Add HelloWorld component below the header */}
-      <main>
-        <Header />
-        <HelloWorld />
-      </main>
-    </div>
+        {/* Add Router functionality around the main content */}
+        <main>
+          <Header />
+          <Routes>
+            <Route path="/" element={<HelloWorld />} />
+            {/* Future components will go here */}
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
 export default App;
-
